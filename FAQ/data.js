d4cb92b6c6d49785ba9d8d795c4795e31026ecc0
Abir <abir.pal899@gmail.com>
2021-01-23 14:33:49 +0530

    Create data.js

---
+++ FAQ/data.js
@@ -0,0 +1,46 @@
+{
+"name":"Hack The Mountains",
+"version":"2.0",
+"FAQ":[
+  {
+    "question":"WHAT IS A HACKATHON?",
+    "answer":"A hackathon is a programming competition in which competitors use technology to build innovative projects within 36 hours. There will be opportunities to win cool prizes, learn from our key-note speakers, and network with our sponsors."
+  },
+  {
+    "question":"WHERE IT WILL BE HOSTED?",
+    "answer":"Hack the mountains 2.0 will be hosted in APRIL, 2020, completely virtually. All you need is to create a Slack account to communicate with our Hack the mountains community."
+  },
+  {
+    "question":"IS IT FREE OF COST?",
+    "answer":"Hack the mountains 2.0 is absolutely free of cost to participate."
+  },
+  {
+    "question":"I AM A BEGINNER. AM I ELIGIBILE TO PARTICIPATE?",
+    "answer":"Yes, this is open to all the people. We would love to have your participation."
+  },
+  {
+    "question":"ARE THERE ANY RULES ?",
+    "answer":"Attendees must abide by the MLH Code of Conduct. We want to respect each other and keep Hack the mountains a safe environment for all participants."
+  },
+  {
+    "question":"WHO CAN PARTICIPATE ?",
+    "answer":"Everyone can participate! We encourage anyone who is interested in learning more about Hackathons, programming, and the tech industry to join Hack the Mountains!"
+  },
+  {
+    "question":"CAN I VOLUNTEER OR BE JUDGE ?",
+    "answer":"Yes! Hack the Mountains wouldn't be possible without our volunteers. We will open mentor and judge sign ups soon, and you can get notified when it opens up."
+  },
+  {
+    "question":"WHAT ARE THE PRIZES FOR WINNERS ?",
+    "answer":"Prizes will be declared soon."
+  },
+  {
+    "question":"What is Duration for the Hackathon?",
+    "answer":"Hack The Mountains is 36 hours online hackathon"
+  },
+  {
+    "question":"How many Members can be  in a Team ?",
+    "answer":"A team consists of 2-5 members. You can also take part solo in this hackathon as well."
+  }
+  ]
+}
